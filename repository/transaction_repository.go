ef91e68fcc0aaf89cc611745e81f8649a6380cb4
badhmanaban.madhu <badhu@ANs-MacBook-Air.local>
2024-06-03 18:35:53 +0530

    badhu: update the feature balance adjustments

--- repository/transaction_repository.go
+++ repository/transaction_repository.go
@@ -18,37 +18,45 @@ type transactionRepo struct {
 	db *gorm.DB
 }
 
-func (t *transactionRepo) AdjustBalance(ctx *gin.Context, transaction *entities.Transaction, dbTxn *gorm.DB) error {
+func (t *transactionRepo) AdjustBalance(ctx *gin.Context, creditTransaction *entities.Transaction, dbTxn *gorm.DB) error {
 	logger := utils.GetLogger(ctx)
 	logger = logger.WithField("Class", "TransactionRepository").
 		WithField("Method", "AdjustBalance")
 
 	// fetch all the transactions with the account number and balance is negative
 	var transactions []entities.Transaction
-	result := dbTxn.Where("account_id = ? AND balance < 0", transaction.AccountID).Find(&transactions)
+	result := dbTxn.Where("account_id = ? AND balance < 0", creditTransaction.AccountID).Find(&transactions)
 	if result.Error != nil {
 		logger.Errorf("error while fetching transactions - %v", result.Error)
 		return result.Error
 	}
 
-	balanceAdjustment := transaction.Balance
-	logger.Info("balance adjustment - ", balanceAdjustment, transaction.Balance)
+	balanceAdjustment := creditTransaction.Amount
 	for _, tx := range transactions {
-		if -1*tx.Balance < balanceAdjustment {
-			balanceAdjustment = tx.Balance + balanceAdjustment
-			tx.Balance = 0
-			logger.Info("tx to update", tx)
+		if balanceAdjustment > 0 {
+			if -1*tx.Balance < balanceAdjustment {
+				balanceAdjustment = tx.Balance + balanceAdjustment
+				tx.Balance = 0
+				if err := t.updateBalance(ctx, dbTxn, tx); err != nil {
+					return err
+				}
+				continue
+			}
+			tx.Balance = tx.Balance + balanceAdjustment
+			balanceAdjustment = 0
 			if err := t.updateBalance(ctx, dbTxn, tx); err != nil {
 				return err
 			}
-			continue
-		}
-		tx.Balance = tx.Balance + balanceAdjustment
-		if err := t.updateBalance(ctx, dbTxn, tx); err != nil {
-			return err
 		}
 	}
 
+	// update the balance of the credit transaction
+	if balanceAdjustment > 0 {
+		creditTransaction.Balance = balanceAdjustment
+		if err := t.updateBalance(ctx, dbTxn, *creditTransaction); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -90,6 +98,12 @@ func (t *transactionRepo) createTransaction(ctx *gin.Context, dbTxn *gorm.DB, tr
 	if result.Error != nil {
 		logger.Errorf("error while inserting transaction - %v", result.Error)
 		return result.Error
+	}
+
+	// Fetch the last inserted record
+	result = dbTxn.Last(transaction)
+	if result.Error != nil {
+		logger.Errorf("error while fetching last inserted record - %v", result.Error)
 	}
 	return nil
 }
